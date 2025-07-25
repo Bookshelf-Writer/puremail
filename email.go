506ce0c3edc969656c64dba626db28df3c74fbb8
SunSung-W541-2025 <mail@embedded.biz.ua>
2025-07-25 13:52:10 +0200

    upd/sync [v0.3.3]  buffering for identical parallel requests

--- email.go
+++ email.go
@@ -1,5 +1,9 @@
 package puremail
 
+import (
+	"golang.org/x/sync/singleflight"
+)
+
 // // // // // // // // // //
 
 type EmailObj struct {
@@ -10,10 +14,36 @@ type EmailObj struct {
 
 //
 
+var newGroup singleflight.Group
+
 func New(mail string) (*EmailObj, error) {
-	return parse(mail, false)
+	v, err, _ := newGroup.Do(mail, func() (any, error) {
+		obj, err := parse(mail, false)
+		if err != nil {
+			return nil, err
+		}
+		return obj, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return v.(*EmailObj), nil
 }
 
 func NewFast(mail string) (*EmailObj, error) {
-	return parse(mail, true)
+	v, err, _ := newGroup.Do(mail, func() (any, error) {
+		obj, err := parse(mail, true)
+		if err != nil {
+			return nil, err
+		}
+		return obj, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return v.(*EmailObj), nil
 }
